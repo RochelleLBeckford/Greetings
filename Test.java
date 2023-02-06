2fd8b3ad572a17bcbc33261c684fee8cd9fb1ec9
RochelleLBeckford <r.beckford@me.com>
2023-02-06 14:48:19 -0500

    initial commit

---
+++ Test.java
@@ -0,0 +1,25 @@
+// class name Must be named in PascalCase -> ie BankAccount-> same name as the file -> file and class name must match
+// Every file is a class
+public class Test {
+
+  // launching point of the application
+  // cannot run a Java program w/o it
+  // public statis void is the samething as the keyword function
+  // main is the name of the method
+  // list params that will use on the outer scope
+    public static void main(String[] args) {
+        // for now code goes here
+        System.out.println("Hello World!");
+    }
+}
+/*`
+~ In Terminal
+-> compile file first -> javac file name
+    -> will see a new folder created -> not human readible [bytecode file]
+-> Invoke the interpreter to human readible code -> how to run your code -> java file name 
+-> then will print out -> see Hello World!
+
+? Must alwayes complie then run every single time 
+Must always compile even if there is a change. Compile then run
+*/
+
